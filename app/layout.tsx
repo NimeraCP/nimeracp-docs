f885c1bc4851c1bf9adf8d4209d0db9920b53d1a
splicho <d.cacian@gmx.de>
2025-03-19 02:52:33 +0100

    chore: Changed Github Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({children}) {
             // banner={banner}
             navbar={navbar}
             pageMap={await getPageMap()}
-            docsRepositoryBase="https://github.com/NimeraCP/nimeracp-docs"
+            docsRepositoryBase="https://github.com/NimeraCP/nimeracp-docs/tree/main/"
             footer={footer}
             // ... Your additional layout options
         >
